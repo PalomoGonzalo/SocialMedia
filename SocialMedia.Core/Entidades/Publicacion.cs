3e1c3097282668a80693f1a8ce1c89de8e252bd1
PalomoGonzalo <89616271+PalomoGonzalo@users.noreply.github.com>
2022-10-12 22:31:09 -0300

    scaffol database

--- SocialMedia.Core/Entidades/Publicacion.cs
+++ SocialMedia.Core/Entidades/Publicacion.cs
@@ -1,14 +1,22 @@
-﻿namespace SocialMedia.Core.Entidades
+﻿using System;
+using System.Collections.Generic;
+
+namespace SocialMedia.Core.Entidades
 {
-    public class Publicacion
+    public partial class Publicacion
     {
-        
-        public int PublicacionId { get; set; }
-        public int UsuarioId { get; set; }
+        public Publicacion()
+        {
+            Comentarios = new HashSet<Comentario>();
+        }
+
+        public int IdPublicacion { get; set; }
+        public int IdUsuario { get; set; }
         public DateTime Fecha { get; set; }
-        public string Descripcion { get; set; }
-        public string Imagen { get; set; }
+        public string Descripcion { get; set; } = null!;
+        public string? Imagen { get; set; }
 
-
+        public virtual Usuario IdUsuarioNavigation { get; set; } = null!;
+        public virtual ICollection<Comentario> Comentarios { get; set; }
     }
 }
